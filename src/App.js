2b44b6aa841dc7c6cfb5c105958b6a755344c491
Jeka <uhp.gonchar.evgeniy@gmail.com>
2020-07-26 21:29:22 +0300

    Add navigation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 import React from 'react';
 import './App.css';
 import NavBar from "./component/NavBar";
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import Main from "./component/page/main/Main";
+import Login from "./component/page/log/Login";
+import Registration from "./component/page/log/Registration";
+import Home from "./component/page/home/Home";
+import createHistory from "history/createBrowserHistory";
+
+const history = createHistory();
 
 function App() {
   return (
-    <div>
+    <BrowserRouter history={history} basename={'/tracking'}>
         <div>
-            Tracking in Ukraine
+            <div>
+                <h1>
+                    Tracking in Ukraine
+                </h1>
+            </div>
+            <div>
+                <NavBar />
+            </div>
+            <div>
+                <Switch>
+                    <Route exact path='/' component={Main} />
+                    <Route exact path='/login' component={Login} />
+                    <Route exact path='/registration' component={Registration} />
+                    <Route exact path='/home' component={Home} />
+                </Switch>
+            </div>
         </div>
-        <div>
-            <NavBar />
-        </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import {NavLink} from "react-router-dom";
 
 const NavBar = () => {
     return (
         <div>
-            NAVBAR
+            <h2>
+                <nav>
+                    <NavLink exact to="/" > Main </NavLink>
+                    <NavLink exact to="/login" > Login </NavLink>
+                    <NavLink exact to="/registration" > Registration </NavLink>
+                    <NavLink exact to="/home" > Home </NavLink>
+                </nav>
+            </h2>
         </div>
     );
 }
