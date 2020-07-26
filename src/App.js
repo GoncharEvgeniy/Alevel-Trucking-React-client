caa118ff1141e79d5acd929ba200d6457dc15781
Jeka <uhp.gonchar.evgeniy@gmail.com>
2020-07-26 20:09:38 +0300

    Add some component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import NavBar from "./component/NavBar";
 
 function App() {
   return (
     <div>
-      APP
+        <div>
+            Tracking in Ukraine
+        </div>
+        <div>
+            <NavBar />
+        </div>
     </div>
   );
 }
